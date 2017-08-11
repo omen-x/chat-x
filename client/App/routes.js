71257b0248b8a8e264d8417666f6b5351c608b34
Denis Volok <denis.omen.x@gmail.com>
2017-08-11 19:49:09 +0300

    routes

--- client/App/routes.js
+++ client/App/routes.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { TransitionGroup } from 'react-transition-group';
 
 import Chat from 'client/Chat'; // eslint-disable-line
 import SignUp from 'client/SignUp'; // eslint-disable-line
+import Login from 'client/Login'; // eslint-disable-line
 import { Auth } from 'modules'; // eslint-disable-line
 
 
 const routes = (
-  <Switch>
-    <Route
-      path="/"
-      exact
-      render={() => (
-        <TransitionGroup>
-          {Auth.isUserAuthenticated() ?
-            <Chat /> :
-            <SignUp />
-          }
-        </TransitionGroup>
-      )}
-    />
-  </Switch>
+  <TransitionGroup>
+    <Switch>
+      <Route
+        path="/"
+        exact
+        render={() => (
+            Auth.isUserAuthenticated() ?
+              <Redirect push to="/chat" /> :
+              <Redirect push to="/signup" />
+
+        )}
+      />
+      <Route path="/chat" component={Chat} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  </TransitionGroup>
 );
 
 export default routes;
